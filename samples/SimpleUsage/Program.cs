d040b783f1f527344bdadf1d14505989e5ba19f1
Ben Collins <aggieben@gmail.com>
2016-07-27 14:21:01 -0500

    update with some package metainfo, and add sample

---
+++ samples/SimpleUsage/Program.cs
@@ -0,0 +1,12 @@
+namespace SimpleUsage
+{
+    using System.IO;
+
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            
+        }
+    }
+}
