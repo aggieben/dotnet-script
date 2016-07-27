fc8aba547366d49b9eb79dc748faf16f685732f3
Ben Collins <aggieben@gmail.com>
2016-07-27 13:02:43 -0500

    stupid-simple first cut at running a script
    Needs lots of things to be practical.  
     - Firstly, need to support some kind of #r syntax like scriptcs.  Can parse those out and add those imports as script options.  For the forseeable future, I'll be focused only on supporting the usage of CoreFX APIs.
     - Commandline argument handling
     - many other things

---
+++ src/BitThicket.DotNet.ScriptTool/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApplication
+{
+    using System.IO;
+    using Microsoft.CodeAnalysis.CSharp.Scripting;
+    using Microsoft.CodeAnalysis.Scripting;
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var scriptText = File.ReadAllText(args[0]);
+
+            var scriptOptions = ScriptOptions.Default.AddImports(
+                "System"
+            );
+
+            var script = CSharpScript.Create(scriptText, scriptOptions);
+
+            script.RunAsync().GetAwaiter().GetResult();
+        }
+    }
+
+    
+}
