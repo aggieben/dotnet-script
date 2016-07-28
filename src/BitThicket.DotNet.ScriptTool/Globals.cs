29bbe04fc176fd688a97073b57500ab66aab7047
Ben Collins <aggieben@gmail.com>
2016-07-28 15:03:19 -0500

    update default assembly references and support Globals

---
+++ src/BitThicket.DotNet.ScriptTool/Globals.cs
@@ -0,0 +1,7 @@
+namespace BitThicket.DotNet.ScriptTool
+{
+    public class Globals
+    {
+        public string[] Args { get; set; }
+    }
+}
--- src/BitThicket.DotNet.ScriptTool/Program.cs
+++ src/BitThicket.DotNet.ScriptTool/Program.cs
@@ -1,23 +1,59 @@
-﻿namespace ConsoleApplication
+﻿namespace BitThicket.DotNet.ScriptTool
 {
+    using System;
     using System.IO;
+    using System.Linq;
+    using System.Reflection;
     using Microsoft.CodeAnalysis.CSharp.Scripting;
     using Microsoft.CodeAnalysis.Scripting;
+
     public class Program
     {
         public static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Print Help Here");
+                return;
+            }
+
             var scriptText = File.ReadAllText(args[0]);
 
-            var scriptOptions = ScriptOptions.Default.AddImports(
-                "System"
-            );
+            var scriptOptions = ScriptOptions.Default
+                .WithReferences(
+                    typeof(System.Object).GetTypeInfo().Assembly,                                           // mscorlib
+                    typeof(System.Runtime.InteropServices.RuntimeInformation).GetTypeInfo().Assembly,       // System.Runtime.?
+                    typeof(System.Collections.Generic.IEnumerable<>).GetTypeInfo().Assembly,                // ?
+                    typeof(System.Console).GetTypeInfo().Assembly,                                          // System.Console
+                    typeof(System.Diagnostics.Trace).GetTypeInfo().Assembly,                                // System.Diagnostics.TraceSource
+                    typeof(System.IO.File).GetTypeInfo().Assembly,                                          // System.IO.FileSystem
+                    typeof(System.Linq.Enumerable).GetTypeInfo().Assembly,                                  // System.Linq
+                    typeof(Newtonsoft.Json.JsonConvert).GetTypeInfo().Assembly                              // Newtonsoft.Json.NetCore
+                );
 
-            var script = CSharpScript.Create(scriptText, scriptOptions);
+            var script = CSharpScript.Create(scriptText, scriptOptions, typeof(Globals));
 
-            script.RunAsync().GetAwaiter().GetResult();
+            new DirectoryInfo("/").GetFiles().FirstOrDefault();
+
+            try
+            {
+                var scriptArgs = new string[args.Length-1];
+                Array.Copy(args, 1, scriptArgs, 0, args.Length-1);
+
+                script.RunAsync(new Globals
+                { 
+                    Args = scriptArgs
+                })
+                .GetAwaiter().GetResult();
+            }
+            catch (CompilationErrorException cee)
+            {
+                Console.Error.WriteLine("Script error: {0}", cee);
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Execution error: {0}", e);
+            }
         }
     }
-
-    
 }
